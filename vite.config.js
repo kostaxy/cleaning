fbc821e7aa4e49128741c67b820f9c21b5a0bc3a
kostaxy <kshandrak@yandex.by>
2025-05-04 22:01:01 +0300

    upd

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,22 @@ export default defineConfig({
     plugins: [react()],
     server: {
         port: 3000,
+        cors: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+            'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+        }
     },
-    base: '',
+    base: './',
     build: {
         outDir: 'dist',
         sourcemap: true,
+        rollupOptions: {
+            output: {
+                manualChunks: undefined
+            }
+        }
     },
     optimizeDeps: {
         include: ['react-router-dom'],
